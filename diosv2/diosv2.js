98e5d5337b6a9598624484ca7b0c0db4ebecb426
Rizsyad AR <42699914+Rizsyad@users.noreply.github.com>
2021-01-11 21:14:04 +0700

    Add features

--- diosv2/diosv2.js
+++ diosv2/diosv2.js
@@ -113,7 +113,8 @@ $(document).ready(function () {
             <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                 <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
                     <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                        <div id="showtable" class="text-white text-lg inline-flex">
+                        <div id="showsDatas">
+                            <div id="showtable" class="text-white text-lg inline-flex">
                             <a id="backdb" class="cursor-pointer mr-3">
                                 Database
                             </a> > <div id="data" class="ml-3"></div>
@@ -128,6 +129,7 @@ $(document).ready(function () {
                             <tbody class="bg-white divide-y divide-gray-200" id="tbody_template">
                             </tbody>
                         </table>
+                        </div>
                         <table class="min-w-full divide-y divide-gray-200" id="output_info">
                             <thead class="bg-gray-50">
                                 <tr>
@@ -257,12 +259,12 @@ $(document).ready(function () {
         $("#output_info").hide()
 
         $("#getInfo").on('click', function () {
-            $("#output").hide()
+            $("#showsDatas").hide()
             $("#output_info").show()
         })
 
         $("#getData").on('click', function () {
-            $("#output").show()
+            $("#showsDatas").show()
             $("#output_info").hide()
         })
 
